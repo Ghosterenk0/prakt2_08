da8e066457e909395309a971e25b5e78c0f50e0c
Ghosterenk0 <arsenijkonkov912@gmail.com>
2025-09-16 15:03:19 +0500

    Add files via upload

---
+++ KonkovPR32-Prakt2/KonkovPR32-Prakt2/KonkovPR32-Prakt2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+
+//Вариант 8
+int main()
+{
+	//zd1
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	for (int i = 4; i >= 0; i--)
+	{
+		std::cout << arr[i] << std::endl;
+	}
+}
+
---
+++ KonkovPR32-Prakt2/zd4/zd4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<math.h>
+
+class Triangle {
+	float side1;
+	float side2;
+	float side3;
+public:
+	Triangle(float s1, float s2, float s3) {
+		side1 = s1;
+		side2 = s2;
+		side3 = s3;
+	}
+	float S() {
+		float p = side1 + side2 + side3;
+		float res = sqrt((p * (p - side1) * (p - side2) * (p - side3)));
+		return res;
+	}
+};
+
+int main()
+{
+	float s1, s2, s3;
+	std::cin >> s1;
+	std::cin >> s2;
+	std::cin >> s3;
+	Triangle tr = Triangle(s1, s2, s3);
+	std::cout << tr.S();
+}
